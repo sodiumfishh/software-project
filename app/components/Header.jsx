13e4e431e6301d1a8a1ff1b3465cad2c0c302c47
Sami Nafis <78338492+sodiumfishh@users.noreply.github.com>
2023-11-04 23:42:01 +0600

    modify: logo link to homepage

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image"
 
 import { SignInButton as SignIn, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
@@ -6,11 +7,13 @@ export default function Header() {
     return (
         <header>
             <div className='flex justify-between items-center container mx-auto sm:px-20 py-4 px-4'>
-                <Image
-                    src="/logo.svg"
-                    alt="Logo"
-                    width={40}
-                    height={40} />
+                <Link href="/">
+                    <Image
+                        src="/logo.svg"
+                        alt="Logo"
+                        width={40}
+                        height={40} />
+                </Link>
                 <nav>
                     <ul className='flex justify-between items-center gap-6'>
                         <SignedOut>
