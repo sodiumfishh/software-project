ad2c5b448c79cf3c38eae17d5643c8266407fb0f
Sami Nafis <78338492+sodiumfishh@users.noreply.github.com>
2023-11-21 11:29:48 +0600

    Add dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -7,6 +7,16 @@ export default async function page() {
     <div className="max-w-7xl mx-auto py-10 px-3">
       <h3 className="text-3xl font-semibold">Hello, {user?.firstName}! 👋</h3>
       <h5 className="text-lg">Upcoming Events</h5>
-  </div>
+
+      <div className="flex justify-center gap-4">
+        <div>
+          <div>
+            <h3 className="text-2xl font-semibold">Today</h3>
+            <hr />
+            <p>No matches found for Tuesday, November 21, 2023</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
